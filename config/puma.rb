22bf48743b64c6fcf2cfe6d3956030364782bcde
BurnerD750 <60318515+BurnerD750@users.noreply.github.com>
2024-08-12 18:17:04 +0900

    Puma bind 0.0.0.0

--- config/puma.rb
+++ config/puma.rb
@@ -25,7 +25,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 # port ENV.fetch("PORT") { 3000 }
-bind "tcp://127.0.0.1:3000"
+bind "tcp://0.0.0.0:3000"
 
 # ssl_bind "127.0.0.1", "3000", {
 #   key: "C:\Users\tchib\vscode\GushikawaDomeRes\server.key",
